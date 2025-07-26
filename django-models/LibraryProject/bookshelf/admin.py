3ac43610fba58ffc3a09e0c4fd9781396b5d11e1
Ms Nyamari <53818120+VivianMoraaNyamari@users.noreply.github.com>
2025-07-27 00:53:15 +0300

    Delete django-models/LibraryProject directory

--- django-models/LibraryProject/bookshelf/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns to display
-    list_filter = ('author', 'publication_year')            # Filters on the right
-    search_fields = ('title', 'author')                     # Search box
